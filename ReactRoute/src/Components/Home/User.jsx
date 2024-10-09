0939afc2101c1eb953b7eff9fd92e2d0102ec24d
mohsinkkc <mohsinkkc1@gmail.com>
2024-10-09 16:52:48 +0530

    add

--- ReactRoute/src/Components/Home/User.jsx
+++ ReactRoute/src/Components/Home/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userid} = useParams()
   return (
-    <div>User : {userid}</div>
+    <div className='bg-gray-600 py-4 text-red text-4xl'>User : {userid}</div>
   )
 }
 
