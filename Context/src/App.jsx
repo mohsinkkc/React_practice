d92a088ff2d09800f20c1dc1699bf525ee40ff14
mohsinkkc <mohsinkkc1@gmail.com>
2024-10-15 16:12:24 +0530

    Context Api Push

---
+++ Context/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Component/Login'
+import Profile from './Component/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>Welcome</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
