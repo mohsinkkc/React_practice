5fd67aac068e655e0431337fa599e034f7c4389f
mohsinkkc <mohsinkkc1@gmail.com>
2024-10-24 15:10:51 +0530

    Add

--- redux_todolist/src/app/store.js
+++ redux_todolist/src/app/store.js
@@ -1,4 +1,7 @@
 import {configureStore} from "@reduxjs/toolKit"
+import todoReducer from '../features/todoSlice'
 
 
-export const store = configureStore({})
+export const store = configureStore({
+    reducer: todoReducer
+})
