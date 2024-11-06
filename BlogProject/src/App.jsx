f77e420f82ce6d3a10735f064742dc665b094f24
mohsinkkc <mohsinkkc1@gmail.com>
2024-11-06 20:10:16 +0530

    Add

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
+import authSerivce from "./appwrite/auth"
 import './App.css'
+import {login,logout} from "./store/authSlice"
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authSerivce.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch (login({userData}))
+      }
+      else{
+        dispatch (logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
 
   return (
     <>
