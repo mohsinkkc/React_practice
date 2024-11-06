6670a9302591ffd3f0c82d271aeb8b097f5fc5ad
mohsinkkc <mohsinkkc1@gmail.com>
2024-11-06 20:31:34 +0530

    Add

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -3,6 +3,7 @@ import {useDispatch} from "react-redux"
 import authSerivce from "./appwrite/auth"
 import './App.css'
 import {login,logout} from "./store/authSlice"
+import {Header,Footer} from "./components"
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -19,13 +20,15 @@ function App() {
     })
     .finally(() => setLoading(false))
   },[])
-
-
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+ 
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap bg-gray-400 content-between'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
