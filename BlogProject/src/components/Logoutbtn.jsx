afb1f827856e84121dffe861a8ed2790ae859afd
mohsinkkc <mohsinkkc1@gmail.com>
2024-11-20 21:54:55 +0530

    Update and Add Code

--- BlogProject/src/components/Logoutbtn.jsx
+++ BlogProject/src/components/Logoutbtn.jsx
@@ -12,8 +12,8 @@ function Logoutbtn() {
         })
     }
   return (
-    <div className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}>Logoutbtn   </div>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}>Logoutbtn   </button>
   )
 }
 
--- BlogProject/src/components/index.js
+++ BlogProject/src/components/index.js
@@ -1,10 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Container } from "../../container/Container";
+import {Logo} from "./Logo";
+import Logoutbtn from "./Logoutbtn";
 
 export{
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    Logoutbtn,
 }
