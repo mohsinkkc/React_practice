207eef7ea017cc2175608d354ce628daa5d332ea
mohsinkkc <mohsinkkc1@gmail.com>
2024-12-02 19:17:57 +0530

    add PostCard

--- BlogProject/src/components/Select.jsx
+++ BlogProject/src/components/Select.jsx
@@ -11,10 +11,14 @@ function Select({
     <div className='w-full'>
         {label && <label htmlFor={id}></label>}
         <select {...props} id={id} ref={ref} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${classname}`}>
-            
+            {options?.map((option, index) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
         </select>
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
